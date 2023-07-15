d908c2de1e2833e8c43d72d73bea5457ba9efc3f
Artur Pedro Martins Neto <87918748+artpedro@users.noreply.github.com>
2023-07-15 20:36:24 -0300

    Código atualizado

--- aula1_viagem_em_grupo.py
+++ aula1_viagem_em_grupo.py
@@ -76,11 +76,12 @@ def imprimir_agenda(agenda):
         ida = voos[(origem, destino)][agenda[id_voo]]
         id_voo += 1
         volta = voos[(destino, origem)][agenda[id_voo]]
-        print('%10s%10s %5s-%5s R$%3s %5s-%5s R$%3s' % (nome, origem, ida[0], ida[1], ida[2],
-                                                       volta[0], volta[1], volta[2]))
-        
+        print(f'{nome}\t{origem}\t{ida[0]}-{ida[1]}\tR${ida[2]}\t{volta[0]}-{volta[1]}\t{volta[2]}')
 agenda = [1,4, 3,2, 7,3, 6,3, 2,4, 5,3]
 imprimir_agenda(agenda)
+
+# Precisamos agora otimizar o algoritmo para encontra
+# o menor valor e menor tempo de espera no a
 
 ## Pesquisa randômica
 
